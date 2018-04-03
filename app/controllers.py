c4b4f382503d8ee97262bba04a5012fd5de0a069
Marcel Lucas <lucas.marcel@epitech.eu>
2018-04-03 20:36:21 +0200

    sign in and out

--- app/controllers.py
+++ app/controllers.py
@@ -26,10 +26,34 @@ class AuthController(object):
         self.user = User(app, conn)
 
     def register_action(self, request):
-        if self.user.user_exists(request.form['username']) == 1:
-            print("User exists")
+        username = request.form['username']
+        password = request.form['password']
+        if (not username.isalnum() and not password.isalnum()):
+            print('invalid: Only alpha-numeric characters are allowed')
         else:
-            self.user.user_create(request.form['username'], request.form['password'])
+            if self.user.user_exists(username) == 1:
+                print("User exists")
+            else:
+                if (len(username) > 0 and len(password)):
+                    self.user.user_create(username, password)
+                else:
+                    print('Invalid: you need to enter the name and the password')
+        return redirect(url_for('route_home'))
+
+    def signin_action(self, request):
+        username = request.form['username']
+        password = request.form['password']
+        if (not username.isalnum() and not password.isalnum()):
+            print('invalid: Only alpha-numeric characters are allowed')
+        else:
+            if (len(username) > 0 and len(password) and self.user.user_exists(username) == 1):
+                print("User exists")
+                return redirect(url_for('route_user_info'))
+            else:
+                print('Invalid: you need to enter the name and the password')
+        return redirect(url_for('route_home'))
+
+    def signout_action(self, request):
         return redirect(url_for('route_home'))
 
 class UserController(object):
