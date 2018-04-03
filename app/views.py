3d41e5165de720419b0e73c7e4b4f7e65fbe6989
Marcel Lucas <lucas.marcel@epitech.eu>
2018-04-03 15:46:57 +0200

    add views

--- app/views.py
+++ app/views.py
@@ -9,14 +9,37 @@ from app import *
 
 ## Index page
 @app.route('/', methods = ['GET'])
-@app.route('/index', methods = ['GET'])
-
-def route_home():
+def route_main():
     UserController().bite()
     return Controller().index_action()
 
-## User pages
-@app.route('/user/<username>', methods = ['POST'])
+##register
+@app.route('/register', methods = ['POST'])
+def route_register():
 
-def route_add_user(username):
-    return "User added !\n"
+##sigin
+@app.route('/sigin', methods = ['POST'])
+def route_sigin():
+
+##sigout
+@app.route('/sigout', methods = ['POST'])
+def route_sigout():
+
+##user
+@app.route('/user', methods = ['GET'])
+def route_user_info():
+
+@app.route('/user/task', methods = ['GET'])
+def route_user_all_task():
+
+@app.route('/user/task/id', methods = ['GET'])
+def route_user_special_task():
+
+@app.route('/user/task/id', methods = ['POST'])
+def route_update_task():
+
+@app.route('/user/task/add', methods = ['POST'])
+def route_create_task():
+
+@app.route('/user/task/del/id', methods = ['POST'])
+def route_delete_task():
