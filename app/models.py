8267321fe0ec332245a3e547188779fbd418d9ed
Cyril COLINET <cyril.colinet@epitech.eu>
2018-04-03 14:21:25 +0200

    :dart: Fix tabulations

--- app/models.py
+++ app/models.py
@@ -11,26 +11,26 @@ from flask_appbuilder import Model
 
 class User(Model):
 
-    __tablename__ = "user"
+        __tablename__ = "user"
 
-    user_id = Column(Integer, primary_key=True)
-    username = Column(String(255), unique=True, nullable=False)
-    password = Column(String(255), nullable=False)
+        user_id = Column(Integer, primary_key=True)
+        username = Column(String(255), unique=True, nullable=False)
+        password = Column(String(255), nullable=False)
 
-    def __repr__(self):
-        return self.username
+        def __repr__(self):
+                return self.username
 
-    def get_username(self):
-        return self.username
+        def get_username(self):
+                return self.username
 
-    def get_user_id(self):
-        return self.user_id
+        def get_user_id(self):
+                return self.user_id
 
-    def user_exists(username):
-        cur = conn.cursor
-        cur.execute("SELECT COUNT(1) FROM %s WHERE username = %s" % (table, username))
-        print(cur.description)
-        print()
-        for row in cur:
-            print(row)
-        cur.close()
+        def user_exists(username):
+                cur = conn.cursor
+                cur.execute("SELECT COUNT(1) FROM %s WHERE username = %s" % (table, username))
+                print(cur.description)
+                print()
+                for row in cur:
+                        print(row)
+                cur.close()
