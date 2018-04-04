13da8de1c3f73d244b633a56b252c9e764731e81
Cyril COLINET <cyril.colinet@epitech.eu>
2018-04-04 15:03:54 +0200

    :tea: Update SQL

--- epytodo.sql
+++ epytodo.sql
@@ -8,8 +8,26 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
+CREATE DATABASE IF NOT EXISTS `epytodo` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE `epytodo`;
 
-DROP TABLE IF EXISTS `user_has_task`;
+CREATE TABLE IF NOT EXISTS `task` (
+  `task_id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `begin` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `end` datetime DEFAULT NULL,
+  `status` enum('not started','in progress','done') NOT NULL DEFAULT 'not started',
+  PRIMARY KEY (`task_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
 CREATE TABLE IF NOT EXISTS `user_has_task` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `fk_user_id` int(11) NOT NULL,
@@ -17,7 +35,7 @@ CREATE TABLE IF NOT EXISTS `user_has_task` (
   PRIMARY KEY (`id`),
   KEY `fk_user_id` (`fk_user_id`),
   KEY `fk_task_id` (`fk_task_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
 ALTER TABLE `user_has_task`
