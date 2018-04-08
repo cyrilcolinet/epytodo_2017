b2cbfb12ea00cf202398c6c25fcc16bbb5d74799
Marcel Lucas <lucas.marcel@epitech.eu>
2018-04-09 00:11:35 +0200

    add none

--- app/controllers.py
+++ app/controllers.py
@@ -100,8 +100,8 @@ class UserController(object):
 
     def create_task_action(self, request):
         title = request.form['title']
-        begin = request.form['begin']
-        end = request.form['end']
+        begin = None
+        end = None
         status = request.form['status']
         result = self.api.task_create(title, begin, end, status)
         ##print(result)
